c6041e987bc63c6fecf17a94f1b3ab6465109009
Egor <egorpetrik2007@gmail.com>
2026-06-23 00:54:29 +0300

    Добавил логику подсчета комбинаций и начал форматировать логику ставок

--- моя фигня/моя фигня.cpp
+++ моя фигня/моя фигня.cpp
@@ -6,19 +6,10 @@
  
 	using namespace std;
 
-	enum class Suit{ clubs , diamonds, hearts, spades};
-	enum Stage { preflop, flop, tern , river, showdown};
+	enum class Suit{ clubs , diamonds, hearts, spades, None};
+	enum Stage { beginning, preflop, flop, tern , river, showdown, };
 	enum class Combinations {
-	OlderCard,
-	Pair,
-	TwoPairs,
-	ThreeofKind,	
-	Straight,
-	Flush,
-	FullHouse,
-	FourofKind,
-	Straight_Flash,
-	Royal_Flash
+		OlderCard, Pair, TwoPairs, ThreeofKind, Straight, Flush, FullHouse, FourofKind, Straight_Flash, Royal_Flash
 	};
 
 	class Card {
@@ -77,9 +68,10 @@
 	class Deck {
 	private:
 		vector <Card> cards;
+		Suit allsuits[4] = { Suit::clubs, Suit::diamonds,Suit::hearts,Suit::spades };
 	public:
 		Deck() {
-			Suit allsuits[] = { Suit::clubs, Suit::diamonds,Suit::hearts,Suit::spades };
+			
 			for (Suit s : allsuits)
 				for (int n = 2; n <= 14; n++)
 					cards.push_back(Card(s, n));
@@ -107,57 +99,66 @@
 	class Player {
 	private:
 		Card Hand[2];
-	
+		int PlayerBudget;
+		string name;
+		bool isFolded = false;
 	public:
+		Player() {
+			setFirstCard(Suit::None, 0);
+			setSecondCard(Suit::None, 0);
+			PlayerBudget = 0;
+		}
 		Player(Card &cart1, Card &cart2) {
 			Hand[0] = cart1;
 			Hand[1] = cart2;
 		}
 
-		void setfc(Suit suit , int numer) {
+		void setSecondCard(Suit suit , int numer) {
 			Hand[1].Setsuit(suit);
 			Hand[1].Setnumber(numer);
 		}
-		void setnc(const Card& cart) { Hand[0] = cart; }
-		void setnc(Suit suit, int numer) {
+		void setFirstCard(const Card& cart) { Hand[0] = cart; }
+		void setFirstCard(Suit suit, int numer) {
 			Hand[0].Setsuit(suit);
 			Hand[0].Setnumber(numer);
 		}
-		void setfc(const Card& cart) { Hand[1] = cart; }
+		void setSecondCard(const Card& cart) { Hand[1] = cart; }
 
 		void printhand() const {
-			cout << "У вас на руках:" << endl << "1 карта ";
+			cout << "У " << name << " на руках : " << endl << "1 карта ";
 			Hand[0].printcard();
 			cout << "2 карта ";
 			Hand[1].printcard();
 		}
 
-		void getfromdeck(Deck& deck) {
-			setnc(deck.dealCard());
-			setfc(deck.dealCard());
+		void getfromdeck(Card& cart1, Card& cart2) {
+			setFirstCard(cart1);
+			setSecondCard(cart2);
 		}
 
 		const Card& getF() const { return Hand[0]; }
 		const Card& getS() const { return Hand[1]; }
+
+		int Raise(int raise) {
+			PlayerBudget -= raise;
+			return raise;
+		}
+		bool Fold() {
+			isFolded = true;
+			return isFolded;
+		}
 	};
 	class Table {
 	private:
-		int pot = 0;
-			Stage stage;
-			unsigned int Numberpl; 
+			int pot = 0;			
 			Card generalcarts[5];        
 			int opCards;         
-
-			struct PlayerSeat {
-				Player* playerPtr = nullptr;
-				bool isFolded = false;
-				int currentBet = 0;
-			};
-			PlayerSeat seats[10];
+			Player seats[10] ;
+			
+			unsigned int Numberpl;
 	public:
 		Table() {
 			Numberpl = 0;
-			stage = preflop;
 			opCards = 0;
 			for (int i = 0; i < 5; i++){
 				generalcarts[i].Setnumber(2);
@@ -166,39 +167,21 @@
 		}
 		void sitPlayer(Player& player) {
 			if (Numberpl < 10) {
-				seats[Numberpl].playerPtr = &player;
+				seats[Numberpl] = player;
 				Numberpl++;
 			}
 			else cout << "Слишком много игроков, нельзя добавить" << endl;
 		}
 
-		void nextstage(Deck &deck) {
-			switch (stage) {
-			case preflop: stage = flop;opCards = 3;
-				for (int i = 0; i < 3;i++) {
-					generalcarts[i] = deck.dealCard();
-				}
-				break;
-			case flop: stage = tern; opCards = 4;
-				generalcarts[opCards - 1] = deck.dealCard(); 
-				break;
-			case tern:stage = river;opCards = 5;
-				generalcarts[opCards-1] = deck.dealCard();
-				break;
-			case river:stage = showdown;
-				break;
-			case showdown: cout << "END OF THE GAME" << endl;
-			}
+		void addGeneralCard(Card &card) {
+			if (opCards > 5) return;
+			generalcarts[opCards - 1] = card;
+			opCards++;
 		}
-		void printTable() const {
+		
+		/*void printTable() const {
 			cout << "Сейчас за столом ";
-			switch (stage) {
-			case preflop:cout << "Префлоп";break;
-			case flop:cout << "Флоп";break;
-			case tern:cout << "Терн";break;
-			case river:cout << "Ривер";break;
-			case showdown:cout << "Шоудаун";break;
-			}
+			
 			cout << ", " << Numberpl << " игроков:" << endl;
 			for (int i = 0;i < Numberpl; i++) {
 				cout << "Игрок " << (i + 1) << " имеет ";
@@ -211,38 +194,253 @@
 				generalcarts[i].printcard();
 			}
 			cout <<endl<< "Сейчас пот на столе " << pot << endl;
-		}
+		}*/
 
 		int getOpCards() const { return opCards; }
+		int getNumofPlayers() const { return Numberpl;  }
+
 		const Card& getGeneralCart(int index) const {
 			return generalcarts[index];
+		}
+		void clearTable(){
+			Numberpl = 0;
+			opCards = 0;
+			for (int i = 0; i < 5; i++) {
+				generalcarts[i].Setnumber(0);
+				generalcarts[i].Setsuit(Suit::None);
+			}
 		}
 	};
 
 	class CombinationCalculator {
-	private:
-		bool cards[4][15] = {};
-		int dignity[15] = {};
-		int countofsuit[4] = {};
-		Combinations combination = Combinations::OlderCard;
+		private:
+			bool cards[4][15] = {};
+			int dignity[15] = {};
+			int countofsuit[4] = {};
 
-		void reset() {
-			fill(&cards[0][0], &cards[0][0] + sizeof(cards) / sizeof(bool), false);
-			fill(begin(dignity), end(dignity), 0);
-			fill(begin(countofsuit), end(countofsuit), 0);
-			combination = Combinations::OlderCard;
-		}
+			Combinations combination = Combinations::OlderCard;
+			vector<int> tieBreakers;
 
-		int getSuitIndex(Suit suit) {
-			switch (suit) {
-			case Suit::clubs:    return 0;
-			case Suit::diamonds: return 1;
-			case Suit::hearts:   return 2;
-			case Suit::spades:   return 3;
-			default:             return 0;
+			int count4 = 0, count3 = 0, count2 = 0;
+			bool isFlush = false;
+			int flushSuit = -1;
+			int straightHigh = -1;
+			int straightFlushHigh = -1;
+
+
+			int getSuitIndex(Suit suit) {
+				switch (suit) {
+				case Suit::clubs:    return 0;
+				case Suit::diamonds: return 1;
+				case Suit::hearts:   return 2;
+				case Suit::spades:   return 3;
+				default:             return 0;
+				}
 			}
-		}
 
+			
+			void analyzeRawData() {
+			
+				for (int i = 0; i < 4; i++) {
+					if (countofsuit[i] >= 5) {
+						isFlush = true;
+						flushSuit = i;
+						break;
+					}
+				}
+
+				for (int i = 14; i >= 2; i--) {
+					if (dignity[i] == 4) count4++;
+					else if (dignity[i] == 3) count3++;
+					else if (dignity[i] == 2) count2++;
+				}
+
+			
+				int straightStreak = 0;
+				int straightFlushStreak = 0;
+
+				for (int i = 14; i >= 1; i--) {
+					int rank = (i == 1) ? 14 : i;
+
+					if (dignity[rank] > 0) {
+						straightStreak++;
+						if (straightStreak >= 5 && straightHigh == -1) {
+							straightHigh = (i == 1) ? 5 : (i + 4);
+						}
+					}
+					else {
+						straightStreak = 0;
+					}
+
+					if (isFlush && cards[flushSuit][rank]) {
+						straightFlushStreak++;
+						if (straightFlushStreak >= 5 && straightFlushHigh == -1) {
+							straightFlushHigh = (i == 1) ? 5 : (i + 4);
+						}
+					}
+					else {
+						straightFlushStreak = 0;
+					}
+				}
+			}
+
+			bool tryRoyalFlush() {
+				if (straightFlushHigh == 14) {
+					combination = Combinations::Royal_Flash;
+					tieBreakers = { 14 };
+					return true;
+				}
+				return false;
+			}
+
+			bool tryStraightFlush() {
+				if (straightFlushHigh != -1) {
+					combination = Combinations::Straight_Flash;
+					tieBreakers = { straightFlushHigh };
+					return true;
+				}
+				return false;
+			}
+
+			bool tryFourOfAKind() {
+				if (count4 > 0) {
+					combination = Combinations::FourofKind;
+					int quads = -1, kicker = -1;
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] == 4) quads = i;
+						else if (dignity[i] > 0 && kicker == -1) kicker = i;
+					}
+					tieBreakers = { quads, kicker };
+					return true;
+				}
+				return false;
+			}
+
+			bool tryFullHouse() {
+				if ((count3 > 0 && count2 > 0) || count3 > 1) {
+					combination = Combinations::FullHouse;
+					int trips = -1, pair = -1;
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] == 3) { trips = i; break; }
+					}
+					for (int i = 14; i >= 2; i--) {
+						if (i != trips && dignity[i] >= 2) { pair = i; break; }
+					}
+					tieBreakers = { trips, pair };
+					return true;
+				}
+				return false;
+			}
+
+			bool tryFlush() {
+				if (isFlush) {
+					combination = Combinations::Flush;
+					for (int i = 14; i >= 2; i--) {
+						if (cards[flushSuit][i] && tieBreakers.size() < 5) {
+							tieBreakers.push_back(i);
+						}
+					}
+					return true;
+				}
+				return false;
+			}
+
+			bool tryStraight() {
+				if (straightHigh != -1) {
+					combination = Combinations::Straight;
+					tieBreakers = { straightHigh };
+					return true;
+				}
+				return false;
+			}
+
+			bool tryThreeOfAKind() {
+				if (count3 > 0) {
+					combination = Combinations::ThreeofKind;
+					int trips = -1;
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] == 3) { trips = i; break; }
+					}
+					tieBreakers.push_back(trips);
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] > 0 && i != trips && tieBreakers.size() < 3) {
+							tieBreakers.push_back(i);
+						}
+					}
+					return true;
+				}
+				return false;
+			}
+
+			bool tryTwoPairs() {
+				if (count2 >= 2) {
+					combination = Combinations::TwoPairs;
+					int pair1 = -1, pair2 = -1;
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] == 2) {
+							if (pair1 == -1) pair1 = i;
+							else if (pair2 == -1) { pair2 = i; break; }
+						}
+					}
+					tieBreakers = { pair1, pair2 };
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] > 0 && i != pair1 && i != pair2) {
+							tieBreakers.push_back(i);
+							break;
+						}
+					}
+					return true;
+				}
+				return false;
+			}
+
+			bool tryPair() {
+				if (count2 > 0) {
+					combination = Combinations::Pair;
+					int pair = -1;
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] == 2) { pair = i; break; }
+					}
+					tieBreakers.push_back(pair);
+					for (int i = 14; i >= 2; i--) {
+						if (dignity[i] > 0 && i != pair && tieBreakers.size() < 4) {
+							tieBreakers.push_back(i);
+						}
+					}
+					return true;
+				}
+				return false;
+			}
+
+			void setOlderCard() {
+				combination = Combinations::OlderCard;
+				for (int i = 14; i >= 2; i--) 
+					if (dignity[i] > 0 && tieBreakers.size() < 5) 
+						tieBreakers.push_back(i);
+			
+			}
+
+			void calculateCards() {
+				analyzeRawData();
+
+				if (tryRoyalFlush())    return;
+				if (tryStraightFlush()) return;
+				if (tryFourOfAKind())   return;
+				if (tryFullHouse())     return;
+				if (tryFlush())         return;
+				if (tryStraight())      return;
+				if (tryThreeOfAKind())  return;
+				if (tryTwoPairs())      return;
+				if (tryPair())          return;
+
+				setOlderCard();
+			}
+
+	public:
+		CombinationCalculator() {
+			tieBreakers.reserve(5);
+		}
+		
 		void addCard(const Card& card) {
 			int suit = getSuitIndex(card.Getsuit());
 			int rank = card.Getnumber();
@@ -251,85 +449,95 @@
 			countofsuit[suit]++;
 		}
 
-	public:
-		CombinationCalculator() { reset(); }
+		void reset() {
+			memset(cards, 0, sizeof(cards));
+			memset(dignity, 0, sizeof(dignity));
+			memset(countofsuit, 0, sizeof(countofsuit));
+			combination = Combinations::OlderCard;
+			tieBreakers.clear();
 
-		void gathercomb(Player& player, const Table& table) {
-			reset();
-			addCard(player.getF());
-			addCard(player.getS());
-			for (int i = 0; i < table.getOpCards(); i++) {
-				addCard(table.getGeneralCart(i));
-			}
+			count4 = count3 = count2 = 0;
+			isFlush = false;
+			flushSuit = -1;
+			straightHigh = -1;
+			straightFlushHigh = -1;
 		}
 
-		void calculateCards() {
-			int count4 = 0, count3 = 0, count2 = 0;
-			bool isFlush = false;
-			int flushSuit = -1;
+		void evaluate(const Card (&playerHand)[2], const Card (&tableCards)[], const int Opcards) {
+			reset(); 
+	
+			for (int i = 0; i < 2; i++) addCard(playerHand[i]);
+			for (int i = 0; i < Opcards; i++) addCard(tableCards[i]);;
 
-			for (int i = 0; i < 4; i++) {
-				if (countofsuit[i] >= 5) {
-					isFlush = true;
-					flushSuit = i;
-					break;
-				}
-			}
-
-			for (int i = 14; i >= 2; i--) {
-				if (dignity[i] == 4) count4++;
-				else if (dignity[i] == 3) count3++;
-				else if (dignity[i] == 2) count2++;
-			}
-
-			bool isStraight = false;
-			bool isStraightFlush = false;
-			bool isRoyalFlush = false;
-
-			int straightStreak = 0;
-			int straightFlushStreak = 0;
-
-			for (int i = 14; i >= 1; i--) {
-				int rank = (i == 1) ? 14 : i;
-
-				if (dignity[rank] > 0) {
-					straightStreak++;
-					if (straightStreak >= 5) isStraight = true;
-				}
-				else {
-					straightStreak = 0;
-				}
-
-				if (isFlush && cards[flushSuit][rank]) {
-					straightFlushStreak++;
-					if (straightFlushStreak >= 5) {
-						isStraightFlush = true;
-						if (i == 10) isRoyalFlush = true;
-					}
-				}
-				else {
-					straightFlushStreak = 0;
-				}
-			}
-
-			if (isRoyalFlush) combination = Combinations::Royal_Flash;
-			else if (isStraightFlush) combination = Combinations::Straight_Flash;
-			else if (count4 > 0) combination = Combinations::FourofKind;
-			else if ((count3 > 0 && count2 > 0) || count3 > 1) combination = Combinations::FullHouse;
-			else if (isFlush) combination = Combinations::Flush;
-			else if (isStraight) combination = Combinations::Straight;
-			else if (count3 > 0) combination = Combinations::ThreeofKind;
-			else if (count2 >= 2) combination = Combinations::TwoPairs;
-			else if (count2 > 0) combination = Combinations::Pair;
-			else combination = Combinations::OlderCard;
+			calculateCards(); 
+		}
+		bool operator==(const CombinationCalculator& other) const {
+			return (this->combination == other.combination && this->tieBreakers == other.tieBreakers);
 		}
 
-		Combinations getCombination() const { return combination; }
+		bool operator!=(const CombinationCalculator& other) const {
+			return !(*this == other);
+		}
+
+		bool operator>(const CombinationCalculator& other) const {
+			if (this->combination != other.combination) {
+				return this->combination > other.combination;
+			}
+			return this->tieBreakers > other.tieBreakers;
+		}
+
+		bool operator<(const CombinationCalculator& other) const {
+			return other > *this;
+		}
 	};
-	class Equity {
+
+
+	class Game {
 	private:
-		double chance;
+		Table table;
+		Stage stage;
+		Deck deck;
 	public:
+		Game() {
+			table = Table();
+			stage = beginning;
+			deck = Deck();
+		}
+		void addPlayer(Player& player) {
+			if (stage == beginning)
+				table.sitPlayer(player);
+			else
+				cout << "Во врея игры нельзя садиться за стол" << endl;
+		}
+		void  
+
+
+
+		/*switch (stage) {
+		case preflop:cout << "Префлоп";break;
+		case flop:cout << "Флоп";break;
+		case tern:cout << "Терн";break;
+		case river:cout << "Ривер";break;
+		case showdown:cout << "Шоудаун";break;
+		}*/
+		/*void nextstage() {
+			switch (stage) {
+			case preflop: stage = flop;table.opCards = 3;
+				for (int i = 0; i < 3;i++) {
+					table.addGeneralCard(deck.dealCard());
+				}
+				break;
+			case flop: stage = tern; opCards = 4;
+				generalcarts[opCards - 1] = deck.dealCard();
+				break;
+			case tern:stage = river;opCards = 5;
+				generalcarts[opCards - 1] = deck.dealCard();
+				break;
+			case river:stage = showdown;
+				break;
+			case showdown: cout << "END OF THE GAME" << endl;
+			}
+		}*/
 	};
 
 	int main()
@@ -339,7 +547,7 @@
 		Deck deck;
 		deck.shuffleDeck();
 
-		Card dummy;
+		/*Card dummy;
 		Player player1(dummy, dummy);
 		Player player2(dummy, dummy);
 
@@ -363,6 +571,6 @@
 		table.printTable();
 		table.nextstage(deck);
 		table.nextstage(deck);
-		table.printTable();
+		table.printTable();*/
 		return 0;
 	}
